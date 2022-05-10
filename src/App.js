148e4c85adb496e3326c0f2681217a4d2123b631
AkashAbrrar <mabrrar9@gmail.com>
2022-05-10 21:48:17 +0600

    this commit for our beautiful banner

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Page/About/About';
 import Home from './Page/Home/Home';
+import Login from './Page/Login/Login';
 import Navbar from './Page/Shared/Navbar';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='about' element={<About></About>}></Route>
+        <Route path='login' element={<Login></Login>}></Route>
       </Routes>
     </div>
   );
--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner/Banner';
 
 const Home = () => {
     return (
         <div>
-            <h2>This is home for stay safe</h2>
+            <Banner></Banner>
         </div>
     );
 };
