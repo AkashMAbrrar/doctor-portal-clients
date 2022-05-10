ae52265bfda10d0e6722748ebc9c591de4c41103
AkashAbrrar <mabrrar9@gmail.com>
2022-05-10 21:00:54 +0600

    this is first commit for navbar and all the files aare added

---
+++ src/Page/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>What do you wanna know about us</h2>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/Page/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>This is home for stay safe</h2>
+        </div>
+    );
+};
+
+export default Home;
