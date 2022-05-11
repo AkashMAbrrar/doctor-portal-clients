c155a6cf435023c365a58b6286cbba45ee202294
AkashAbrrar <mabrrar9@gmail.com>
2022-05-11 23:14:02 +0600

    this is for appointment section

---
+++ src/Page/Home/Banner/MakeAppointment.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import doctor from '../../../assets/images/doctor-small.png';
+import backApp from '../../../assets/images/appointment.png';
+import MainButton from '../../Shared/MainButton';
+
+const MakeAppointment = () => {
+    return (
+        <section style={{
+            background: `url(${backApp})`
+        }}
+            className='flex pt-10 justify-center items-center mt-28'>
+            <div className='flex-1 hidden lg:block'>
+                <img className='mt-[-100px]' src={doctor} alt="" />
+            </div>
+            <div className='flex-1'>
+                <h1 className='font-bold text-2xl text-primary'>Appointment</h1>
+                <h1 className='text-4xl text-bold text-white'>Make An Appointment Today</h1>
+                <p className='text-white'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                <MainButton>Get Start</MainButton>
+            </div>
+        </section>
+    );
+};
+
+export default MakeAppointment;
--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import Info from './Banner/Info';
+import MakeAppointment from './Banner/MakeAppointment';
 import DentalInfo from './DentalInfo/DentalInfo';
 import Services from './Services/Services';
 
@@ -12,6 +13,7 @@ const Home = () => {
             <Info></Info>
             <Services></Services>
             <DentalInfo></DentalInfo>
+            <MakeAppointment></MakeAppointment>
         </div>
     );
 };
