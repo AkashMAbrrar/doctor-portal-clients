90d39a80da37efca920418fe824799c45cf8526e
AkashAbrrar <mabrrar9@gmail.com>
2022-05-12 23:04:13 +0600

    modal is added for services deatail and make by daisy ui appi

--- src/Page/Appointment/Service.js
+++ src/Page/Appointment/Service.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Service = ({ service }) => {
+const Service = ({ service, setTreatment }) => {
     const { name, slots } = service;
     return (
         <div class="card lg:max-w-lg bg-base-100 shadow-xl">
@@ -13,7 +13,12 @@ const Service = ({ service }) => {
                 }</p>
                 <p className='text-center'>{slots.length} {slots.length > 1 ? 'Spaces' : 'Space'} Available</p>
                 <div class="card-actions justify-center">
-                    <button disabled={slots.length === 0} class="btn btn-secondary text-white">Book Appointment</button>
+                    <label
+                        for="booking-modal"
+                        disabled={slots.length === 0}
+                        onClick={() => setTreatment(service)}
+                        className="btn modal-button btn btn-secondary text-white">Book Appointment</label>
+
                 </div>
             </div>
         </div>
