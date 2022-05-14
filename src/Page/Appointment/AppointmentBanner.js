b13e0212fa6d55c5a1ff2eef6cef9ef8c238d4b7
AkashAbrrar <mabrrar9@gmail.com>
2022-05-14 23:32:25 +0600

    this commit for google auth provider

--- src/Page/Appointment/AppointmentBanner.js
+++ src/Page/Appointment/AppointmentBanner.js
@@ -8,9 +8,9 @@ const AppointmentBanner = ({ date, setDate }) => {
     return (
         <div style={{
             background: `url(${bgChair})`
-        }} class="hero min-h-screen bg-base-200">
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt='' />
+        }} className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt='' />
                 <div>
                     <DayPicker
                         mode="single"
--- src/Page/Appointment/Service.js
+++ src/Page/Appointment/Service.js
@@ -3,16 +3,16 @@ import React from 'react';
 const Service = ({ service, setTreatment }) => {
     const { name, slots } = service;
     return (
-        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-            <div class="card-body text-center">
-                <h2 class="text-xl font-bold text-secondary text-center">{name}</h2>
+        <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+            <div className="card-body text-center">
+                <h2 className="text-xl font-bold text-secondary text-center">{name}</h2>
                 <p className='text-center'>{
                     slots.length > 0
                         ? <span>{slots[0]}</span>
                         : <span className='text-red-500'>Sorry ! No Slots Available</span>
                 }</p>
                 <p className='text-center'>{slots.length} {slots.length > 1 ? 'Spaces' : 'Space'} Available</p>
-                <div class="card-actions justify-center">
+                <div className="card-actions justify-center">
                     <label
                         for="booking-modal"
                         disabled={slots.length === 0}
